bd45512da22f6669b7259d8baf6f144996a136c8
letchauu <leticiasilva203@outlook.com>
2025-06-12 11:25:26 -0300

    aula 12/06

---
+++ php/aula1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+<body>
+    <?php
+    # primeiro código
+    echo "Olá Mundo!";
+    // imprime a mensagem Olá Mundo na tela
+    /* comentários em linha podem ser feitos com os símbolos # e //, já os comentários com mais de uma linha devem ser feitos com os sinais /* e */
+?>
+</body>
+</html>
